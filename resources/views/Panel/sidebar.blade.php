1faf0d4b10bef166d561fbf6490293fede0d3361
Bhanu Prakash <prakash.1024@yahoo.com>
2024-01-30 23:08:31 +0530

    title modified

--- resources/views/Panel/sidebar.blade.php
+++ resources/views/Panel/sidebar.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} | {{$title}}</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Styles -->
