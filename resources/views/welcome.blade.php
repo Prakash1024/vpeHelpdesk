da50e8ded5122d38f50d0e1302265a952fc97053
Bhanu Prakash <prakash.1024@yahoo.com>
2024-01-30 23:12:32 +0530

    title modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }} | {{$title}}</title>
+        <title>{{ config('app.name', 'Laravel') }} @isset($title) | {{$title}} @endisset</title>
         @vite(['resources/sass/app.scss',
         'resources/js/app.js',])
         <!-- Fonts -->
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome')->with('title', 'Home');;
+    return view('welcome')->with('title', 'Home');
 });
