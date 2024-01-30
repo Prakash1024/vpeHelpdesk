aaa481a2bf8b01d87e0eb7167a394c1d6faa1d3c
Bhanu Prakash <prakash.1024@yahoo.com>
2024-01-30 22:54:45 +0530

    added favicon

---
+++ resources/views/Panel/sidebar.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Styles -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js',])
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('vpe.ico') }}" sizes="16x16" type="image/x-icon">
+    <link rel="icon" href="{{ asset('vpe.ico') }}" sizes="32x32" type="image/x-icon">
+    <link rel="icon" href="{{ asset('vpe.ico') }}" sizes="48x48" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('vpe.ico') }}" type="image/x-icon">
+</head>
+<body>
+
+</body>
+</html>
