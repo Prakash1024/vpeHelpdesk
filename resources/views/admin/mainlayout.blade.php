d1765078d5c3c965978ba7995c55f67045938987
Bhanu Prakash <prakash.1024@yahoo.com>
2024-02-20 21:39:05 +0530

    sidebar added

--- resources/views/admin/mainlayout.blade.php
+++ resources/views/admin/mainlayout.blade.php
@@ -15,10 +15,61 @@
     <link rel="shortcut icon" href="{{ asset('vpe.ico') }}" type="image/x-icon">
     <!--Style Sheets-->
     <link href="{{ asset('css/mainstyles.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/sidebar.js') }}"></script>
+
 </head>
 <body>
-    <nav class="aside">
-    </nav>
+    <body id="body-pd">
+        <header class="header" id="header">
+            <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+            <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
+        </header>
+        <div class="l-navbar" id="nav-bar">
+            <nav class="nav">
+                <div>
+                  <a href="#" class="nav_logo">
+                    <img src="{{ asset('vpe.ico') }}" alt="VPE" width="30" height="24">
+                    <span class="nav_logo-name">Customer Care</span>
+                  </a>
+                  <div class="nav_list">
+                    <a href="#" class="nav_link active">
+                      <i class='bx bx-grid-alt nav_icon'></i>
+                      <span class="nav_name">Dashboard</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                      <i class='bx bx-user nav_icon'></i>
+                      <span class="nav_name">Users</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                      <i class='bx bx-message-square-detail nav_icon'></i>
+                      <span class="nav_name">Messages</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                      <i class='bx bx-bookmark nav_icon'></i>
+                      <span class="nav_name">Bookmark</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                      <i class='bx bx-folder nav_icon'></i>
+                      <span class="nav_name">Files</span>
+                    </a>
+                    <a href="#" class="nav_link">
+                      <i class='bx bx-bar-chart-alt-2 nav_icon'></i>
+                      <span class="nav_name">Stats</span>
+                    </a>
+                  </div>
+                </div>
+                <a href="#" class="nav_link">
+                  <i class='bx bx-log-out nav_icon'></i>
+                  <span class="nav_name">SignOut</span>
+                </a>
+              </nav>
+        </div>
+        <!--Container Main start-->
+        <div class="height-100 bg-light">
+            <h4>Main Components</h4>
+        </div>
+        <!--Container Main end-->
     <section>
         <div class="main-header">
         </div>
@@ -29,3 +80,5 @@
     </section>
 </body>
 </html>
+
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css" rel="stylesheet">
