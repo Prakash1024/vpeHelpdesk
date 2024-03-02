ccee598585f1967abfeca71ae8ec08e44c0a64b4
bhanu <prakash.1024@gmail.com>
2024-03-02 13:32:57 +0530

    added noindex/nofollow

--- resources/views/admin/mainlayout.blade.php
+++ resources/views/admin/mainlayout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow" />
     <title>{{ config('app.name', 'Laravel') }} @isset($title) | {{$title}} @endisset</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
